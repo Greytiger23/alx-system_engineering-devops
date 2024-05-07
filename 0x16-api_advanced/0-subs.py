e5a3cf37cb91861359ae40b1f953cd9342b62fa3
Greytiger23 <isabelmothoa49@gmail.com>
2024-05-07 10:27:51 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom User Agent'}
-    a = requests.get(url, headers=headers)
+    a = requests.get(url, headers=headers, allow_redirects=False)
     if a.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
