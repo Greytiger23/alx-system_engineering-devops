9a0353c84f83923091ae8fa409eab721ce8105c0
Greytiger23 <isabelmothoa49@gmail.com>
2024-05-08 17:58:06 -0700

    web stack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,4 @@
 # Puppet manifest to fix apache 500 error
-file { '/etc/apache2/sites-available/apache2.conf':
-  ensure  => file,
-  content => file('/etc/apache2/apache2.conf'),
-}
-
-service { 'apache2':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/apache2/sites-available/apache2.conf'],
-} 
-
 exec { 'fix_apache':
   command     => '/bin/sed -i "s/SomeIncorrectDirective/CorrectDirective/g" /etc/apache2/apache2.conf',
   path        => ['/bin', '/usr/bin'],
